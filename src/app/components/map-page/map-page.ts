1758855c4ab22000b6cbac182b394ca71ecd6a4d
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-12 18:45:16 -0400

    feat: se agrego carpeta assets

--- src/app/components/map-page/map-page.ts
+++ src/app/components/map-page/map-page.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'assets/marker-icon-2x.png',
+  iconUrl: 'assets/marker-icon.png',
+  shadowUrl: 'assets/marker-shadow.png',
+});
+
 @Component({
   selector: 'app-map-page',
   standalone: true,
