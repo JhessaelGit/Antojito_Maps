d3bd48d61dcec3b0605b4549249ca266f5876d66
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-12 22:44:39 -0400

    fix: se cambio de lugar los assets en publico

--- src/app/components/map-page/map-page.ts
+++ src/app/components/map-page/map-page.ts
@@ -4,9 +4,9 @@ import * as L from 'leaflet';
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'assets/marker-icon-2x.png',
-  iconUrl: 'assets/marker-icon.png',
-  shadowUrl: 'assets/marker-shadow.png',
+  iconRetinaUrl: '/assets/marker-icon-2x-red.png',
+  iconUrl: '/assets/marker-icon-red.png',
+  shadowUrl: '/assets/marker-shadow.png'
 });
 
 @Component({
