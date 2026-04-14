86ebd3a79998947c45af2ff476f05f63130b215a
Tomas Claros <sergio.claros@ucb.edu.bo.com>
2026-04-14 00:04:52 -0400

    Se implemento el traductor a la pagina de crear administrador

--- src/app/components/admin-create/admin-create.ts
+++ src/app/components/admin-create/admin-create.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoggerService } from '../../core/services/logger.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-admin-create',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TranslateModule],
   templateUrl: './admin-create.html',
   styleUrl: './admin-create.css'
 })
@@ -25,7 +26,8 @@ export class AdminCreate {
 
   constructor(
     private router: Router,
-    private logger: LoggerService
+    private logger: LoggerService,
+    private translate: TranslateService
   ) {}
 
   onFileSelected(event: any) {
@@ -42,22 +44,21 @@ export class AdminCreate {
   }
 
   agregar() {
-
     this.clearErrors();
     let valid = true;
 
     if (!this.nombre.trim()) {
-      this.errorNombre = 'Nombre requerido';
+      this.errorNombre = this.translate.instant('ADMIN_CREATE.ERR_NAME');
       valid = false;
     }
 
     if (!this.correo.includes('@')) {
-      this.errorCorreo = 'Correo inválido';
+      this.errorCorreo = this.translate.instant('ADMIN_CREATE.ERR_EMAIL');
       valid = false;
     }
 
     if (!this.funciones.trim()) {
-      this.errorFunciones = 'Funciones requeridas';
+      this.errorFunciones = this.translate.instant('ADMIN_CREATE.ERR_FUNCTIONS');
       valid = false;
     }
 
@@ -75,7 +76,7 @@ export class AdminCreate {
       email: this.correo
     });
 
-    this.router.navigate(['/admin']); // volver a lista
+    this.router.navigate(['/admin']); 
   }
 
   clearErrors() {
