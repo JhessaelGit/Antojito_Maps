8c033c86ff858b1ce16bb6110d78098602d190e5
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-05-08 17:52:03 -0400

    feat: create new component admin request and request detail modal

---
+++ src/app/components/request-detail-modal/request-detail-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-detail-modal',
+  imports: [],
+  templateUrl: './request-detail-modal.html',
+  styleUrl: './request-detail-modal.css',
+})
+export class RequestDetailModal {
+
+}
