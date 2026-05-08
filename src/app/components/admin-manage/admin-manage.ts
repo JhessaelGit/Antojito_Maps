94c04324c71d150d38f0f126d20aa0e38d954b7f
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-05-08 19:04:47 -0400

    feat(admin): add admin management section

--- src/app/components/admin-manage/admin-manage.ts
+++ src/app/components/admin-manage/admin-manage.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-manage',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './admin-manage.html',
-  styleUrl: './admin-manage.css',
+  styleUrl: './admin-manage.css'
 })
-export class AdminManage {
+export class AdminManageComponent {
+
+  constructor(
+    private router: Router,
+    private location: Location
+  ) {}
+
+  volverAtras(): void {
+    this.location.back();
+  }
+
+  irAgregar(): void {
+    this.router.navigate(['/admin/agregar']);
+  }
+
+  irEditar(): void {
+    this.router.navigate(['/admin/editar']);
+  }
+
+  irEliminados(): void {
+    this.router.navigate(['/admin/eliminados']);
+  }
 
 }
